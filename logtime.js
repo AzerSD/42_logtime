3c0e21ddfe34c4607956ac22260b08c78a3656f0
Azer Sioud <sioudazer8@gmail.com>
2023-08-11 15:49:20 +0200

    crepou's changes

--- logtime.js
+++ logtime.js
@@ -1,19 +1,14 @@
 const monthLabels = Array.from(document.querySelectorAll('svg text[font-size="10"]'));
 const months = {};
-
 let currentMonthLabel = null;
-
 monthLabels.forEach(label => {
   const monthName = label.textContent.trim();
   months[monthName] = [];
   currentMonthLabel = monthName;
 });
-
 const elements = document.querySelectorAll('svg text[font-size="10"], svg g[data-toggle="tooltip"][data-original-title]');
-
 elements.forEach(element => {
   const tagName = element.tagName.toLowerCase();
-
   if (tagName === 'text') {
     const monthName = element.textContent.trim();
     currentMonthLabel = monthName;
@@ -23,19 +18,16 @@ elements.forEach(element => {
     }
   }
 });
-
 const monthSums = {};
-
 for (const monthName in months) {
   monthSums[monthName] = 0;
-
   months[monthName].forEach(element => {
     const title = element.getAttribute('data-original-title');
-    const time = parseFloat(title);
-
+    const time = parseFloat(title.replace("h","."));
     if (!isNaN(time)) {
       monthSums[monthName] += time;
     }
   });
+  monthSums[monthName] = (monthSums[monthName] + ((monthSums[monthName] - Math.floor(monthSums[monthName])) * 0.6)).toFixed(2);
 }
 monthSums;
